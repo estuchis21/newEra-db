6b6a9d15cc2f743142b04d16f8a593cc232c7233
estuchis01 <estebansoft.dev2001@gmail.com>
2026-07-20 20:19:45 -0300

    auth hecha

--- ALUMNOS - GRUPOS - ASISTENCIAS - CLASES/INSERT/sp_registro.sql
+++ ALUMNOS - GRUPOS - ASISTENCIAS - CLASES/INSERT/sp_registro.sql
@@ -1,73 +1,29 @@
 CREATE OR REPLACE PROCEDURE RegistroAlumno (
-    data_usuarios alumno_input,
-    alumnos alumnos_type,
-    profesores profesorestype,
-    OUT id_usuario_creado INT
+	data_alumnos alumno_input
 )
 
 LANGUAGE plpgsql
 AS $$
-
 BEGIN
-
-    INSERT INTO users(
+	INSERT INTO users(
         nombre,
         apellido,
         dni,
         email,
-        contrasena,
+		contrasena,
         username,
         celular,
-        id_rol
+		id_rol
     )
     VALUES(
-        data_usuarios.nombre,
-        data_usuarios.apellido,
-        data_usuarios.dni,
-        data_usuarios.email,
-        data_usuarios.contrasena,
-        data_usuarios.username,
-        data_usuarios.celular,
-        data_usuarios.id_rol
-    )
-    RETURNING id_usuario
-    INTO id_usuario_creado;
-
-
-
-    IF data_usuarios.id_rol = 1 THEN
-
-        INSERT INTO alumnos (
-            id_usuario,
-            es_menor
-        )
-        VALUES (
-            id_usuario_creado,
-            alumnos.es_menor
-        );
-
-    END IF;
-
-
-
-    IF data_usuarios.id_rol = 2 THEN
-
-        INSERT INTO profesores (
-            id_usuario
-        )
-        VALUES (
-            id_usuario_creado
-        );
-
-    END IF;
-
-
-
-EXCEPTION
-
-    WHEN OTHERS THEN
-
-        RAISE;
+        data_alumno.nombre,
+        p_al.apellido,
+        p_alumno.dni,
+        p_alumno.email,
+        p_alumno.contrasena,
+		p_alumno.username,
+        p_alumno.celular
+    );
 
 END;
 $$;
